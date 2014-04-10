66c2ea91cf4f39f1d8a4b8bb9b4d1e851ce5a0e6
Адонай <adonai@xaker.ru>
2014-04-10 17:53:10 +0400

    - description of new sync process

--- wallet-server/server/syncclientsocket.h
+++ wallet-server/server/syncclientsocket.h
@@ -13,6 +13,37 @@
 namespace sync = com::adonai::wallet::sync;
 namespace pbuf = google::protobuf;
 
+/**
+ * @brief The SyncClientSocket class
+ *
+ * This class provides full sync process between server and client
+ *
+ * 1) Client sends sync auth/register packet. Server responds with convenient answer
+ * 2) Client sends known server ids and last revision known to him
+ *
+ * Server stores last revision of data by specified account
+ * Server stores last modification revision along with any entity received from client
+ *
+ * If outdated device requests sync, server compares its last revision number with current revision for this account
+ * After that server can select objects with last revision > last client revision.
+ * That are added and modified objects.
+ * Server also creates a list of deleted objects' IDs as (knownToClientIDs subtract presentOnServerIDs)
+ *
+ * 3) Server sends added and modified objects to client. Server also sends deleted entities' IDs.
+ *
+ * Client is meant to be able to recoincile all the data from server - this is important part! In short this process looks like `git rebase`
+ * a) Client adds received objects locally
+ * b) Client overwrites its objects with modified on server
+ * c) Client deletes local objects that do not exist on server (bound objects are deleted also...)
+ *
+ * d) Client reapplies all the local non-synced changes
+ *
+ * 4) Client sends pack of its added, modified, deleted data to server
+ * 5) Server modifies its data and sends confirmation (with IDs) to client
+ * 6) Sync is finished
+ *
+ */
+
 class SyncClientSocket : public QTcpSocket
 {
     Q_OBJECT
