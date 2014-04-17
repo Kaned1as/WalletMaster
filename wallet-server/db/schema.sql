6abe76a84507925b87a96b24ce2c2c2bb20c3e9d
Адонай Элохим <algonkvel@gmail.com>
2014-04-17 08:08:20 +0400

    - altered db schema

--- wallet-server/db/schema.sql
+++ wallet-server/db/schema.sql
@@ -18,6 +18,29 @@ USE `wallet`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id of synced account - represents GUID in client database',
+  `sync_account` int(11) NOT NULL COMMENT 'sync account, bound by foreign key to table sync_accounts',
+  `name` varchar(45) NOT NULL COMMENT 'name of synced account - equals name in client DB',
+  `description` varchar(45) DEFAULT NULL COMMENT 'description of synced account - equals description in client DB',
+  `currency` varchar(10) DEFAULT NULL COMMENT 'currency of synced account - equals currency in client DB',
+  `amount` varchar(45) NOT NULL COMMENT 'amount of synced account - equals amount in client DB',
+  `color` int(11) DEFAULT NULL COMMENT 'color of synced account - equals color in client DB',
+  `last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `account_idx` (`sync_account`),
+  CONSTRAINT `account` FOREIGN KEY (`sync_account`) REFERENCES `sync_accounts` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=695 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `sync_accounts`
 --
 
@@ -41,4 +64,4 @@ CREATE TABLE `sync_accounts` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-04-04  1:20:18
+-- Dump completed on 2014-04-17  8:07:16
