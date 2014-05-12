93f5987aaca29abfabd32a2e45875cc4cf477a94
Адонай Элохим <adonai@xaker.ru>
2014-05-12 08:31:24 +0400

    update db schema

--- wallet-server/db/schema.sql
+++ wallet-server/db/schema.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `wallet` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `wallet`;
--- MySQL dump 10.13  Distrib 5.5.35, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.37, for debian-linux-gnu (x86_64)
 --
 -- Host: 127.0.0.1    Database: wallet
 -- ------------------------------------------------------
--- Server version	5.5.35-2
+-- Server version	5.5.37-1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `accounts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `accounts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id of synced account - represents GUID in client database',
+  `id` varchar(45) NOT NULL COMMENT 'id of synced account - represents GUID in client database',
   `sync_account` int(11) NOT NULL COMMENT 'sync account, bound by foreign key to table sync_accounts',
   `name` varchar(45) NOT NULL COMMENT 'name of synced account - equals name in client DB',
   `description` varchar(45) DEFAULT NULL COMMENT 'description of synced account - equals description in client DB',
@@ -33,11 +33,36 @@ CREATE TABLE `accounts` (
   `amount` varchar(45) NOT NULL COMMENT 'amount of synced account - equals amount in client DB',
   `color` int(11) DEFAULT NULL COMMENT 'color of synced account - equals color in client DB',
   `last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
+  PRIMARY KEY (`id`,`sync_account`),
   KEY `account_idx` (`sync_account`),
   CONSTRAINT `account` FOREIGN KEY (`sync_account`) REFERENCES `sync_accounts` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=695 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `operations`
+--
+
+DROP TABLE IF EXISTS `operations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `operations` (
+  `id` varchar(45) NOT NULL,
+  `sync_account` int(11) NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `amount` varchar(45) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `charger_id` varchar(45) DEFAULT NULL,
+  `beneficiar_id` varchar(45) DEFAULT NULL,
+  `converting_rate` double DEFAULT NULL,
+  `last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`,`sync_account`),
+  KEY `charger_idx` (`charger_id`),
+  KEY `beneficiar_idx` (`beneficiar_id`),
+  CONSTRAINT `beneficiar_fk` FOREIGN KEY (`beneficiar_id`) REFERENCES `accounts` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `charger_fk` FOREIGN KEY (`charger_id`) REFERENCES `accounts` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -52,7 +77,7 @@ CREATE TABLE `sync_accounts` (
   `login` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -64,4 +89,4 @@ CREATE TABLE `sync_accounts` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-04-17  8:07:16
+-- Dump completed on 2014-05-12  8:30:17
